d4d66bdeef58f64279bf5dcebfd029e9fe19a966
Wealthometer <wealthhajoh87@gmail.com>
2025-07-09 20:03:54 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
+import { Greeting } from './Greeting';
 import './App.css';
 
 function App() {
+  let adjective = "COOL";
+  let url = "https://reactjs.org"
+
   return (
     <div className="App">
       <header className="App-header">
+        <Greeting name = "Wealth" numberOfMessages={100} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>I Am So Awesome</h1>
+          <h1>This is so {adjective}</h1>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
